472d73db5e987f75d6936a6d45e42baac8473729
Mike MacAdam <mikemacadam87@gmail.com>
2016-09-25 13:33:26 -0700

    checkpoint 19 done

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21' },
+         { title: 'Magenta', duration: '2:15' }
+     ]
+ };
+ 
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21' },
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15' }
+     ]
+ };
+
+var albumDragon = {
+     title: 'The Dragon',
+     artist: 'King Aurther',
+     label: 'Medieval',
+     year: '1242',
+     albumArtUrl: 'assets/images/album_covers/09.png',
+     songs: [
+         { title: 'Hear my roar', duration: '3:23' },
+         { title: 'The Princess is mine', duration: '2:56' },
+         { title: 'Fire Breather', duration: '3:10'},
+         { title: 'Dark Sky', duration: '2:14' },
+         { title: 'Gold Hoard', duration: '1:59'}
+     ]
+ };
