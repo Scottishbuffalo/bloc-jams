94f36d969168af7c63774b98369988fc0cceaa48
Mike MacAdam <mikemacadam87@gmail.com>
2016-09-25 14:04:26 -0700

    assignment 19 complete

--- scripts/album.js
+++ scripts/album.js
@@ -1,3 +1,14 @@
+var setSong = function(song){
+    currentlyPlayingSongNumber = parseInt(songNumber)
+    currentSongFromAlbum = currentAlbum.songs[songNumber - 1]
+}
+
+
+var getSongNumberCell = function(number){
+    return $('.song-item-number[data-song-number="' + number + '"]');
+}
+
+
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -13,12 +24,12 @@ var createSongRow = function(songNumber, songName, songLength) {
          //var songNumber = $(this).attr('data-song-number');
          var songNumber = parseInt($(this).attr('data-song-number'));
 	   if (currentlyPlayingSongNumber !== null) {
-		  var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+		  var currentlyPlayingCell = getSongNumberCell;
 		  currentlyPlayingCell.html(currentlyPlayingSongNumber);
 	   }
 	   if (currentlyPlayingSongNUmber !== songNumber) {
 		  $(this).html(pauseButtonTemplate);
-		  currentlyPlayingSongNumber = songNumber;
+		  setSong(songNumber)
             currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
             updatePlayerBarSong();
 	   } else if (currentlyPlayingSongNumber === songNumber) {
